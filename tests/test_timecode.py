e4f95e7f4e18de9cc642cfcb0f58fd581769b096
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-01-01 15:54:54 +0000

    - Updated documentation to Google Docstring format.

--- tests/test_timecode.py
+++ tests/test_timecode.py
@@ -418,7 +418,9 @@ def test_add_with_non_suitable_class_instance(args, kwargs, func, tc2):
     with pytest.raises(TimecodeError) as cm:
         _ = func(tc1, tc2)
 
-    assert "Type {} not supported for arithmetic.".format(tc2.__class__.__name__) == str(cm.value)
+    assert str(cm.value) == "Type {} not supported for arithmetic.".format(
+        tc2.__class__.__name__
+    )
 
 
 def test_div_method_working_properly_1():
